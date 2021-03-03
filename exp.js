14503aca8a80e8d710dd6c44f6d33478f21d840b
Prakshal Jain <prakshal@buffalo.edu>
2021-03-03 04:59:36 +0000

    Update exp.js

--- exp.js
+++ exp.js
@@ -463,7 +463,8 @@ for (let i = 0; i < length; i++) {//loop through the silmuli list
                 choices: ['Low', 'High'],
                 prompt: "<p>Is the pitch high or low?</p>"
             };
-            timeline.push(trial);
+            trial;
+            timeline.push(text_response);
         }
 
     }
