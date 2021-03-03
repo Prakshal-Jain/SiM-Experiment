3becf12d86b5a33adb1c2ba003d51376b07be121
Prakshal Jain <prakshal@buffalo.edu>
2021-03-03 06:52:13 +0000

    Update exp.js

--- exp.js
+++ exp.js
@@ -437,7 +437,7 @@ for (let i = 0; i < length; i++) {//loop through the silmuli list
             var text_response = {
                 type: 'audio-text-response',
                 stimulus: stimuli_list[i].stimulus,
-                replay: true,
+                replay: false,
                 autoplay: true,
                 //require_movement: true,
                   questions: [
