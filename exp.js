caed5bbd2c09cea58443856ef62307423e27be21
Prakshal Jain <prakshal@buffalo.edu>
2021-03-08 07:13:39 +0000

    Update exp.js

--- exp.js
+++ exp.js
@@ -318,6 +318,7 @@ var toggle_audio = {
     stimulus: '<p>Please choose how do you want to toggle the audio in the experiment.</p>',
     choices: ['Play all audio files', 'Play psudorandom files'],
     on_finish: function (data) {
+        conslole.log("pushing psudo to timeline");
         timeline.push(psudo);
     }
 };
