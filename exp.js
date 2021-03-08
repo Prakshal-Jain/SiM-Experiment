01a64909ca82b68c47b8ef59973bc5490e5418ed
Prakshal Jain <prakshal@buffalo.edu>
2021-03-08 08:22:04 +0000

    Update exp.js

--- exp.js
+++ exp.js
@@ -25,16 +25,6 @@ if (checkiPad == "iPad" || String(browser.tablet) !== "undefined" || String(brow
     close();
 }
 
-/* create timeline */
-var timeline = [];
-
-/* init connection with pavlovia.org */
-var pavlovia_init = {
-    type: "pavlovia",
-    command: "init"
-};
-timeline.push(pavlovia_init);
-
 /* Get stimuli from stimuli.json created by files_to_json.py */
 var stimuli_list = [];
 $.ajax({
@@ -46,41 +36,6 @@ $.ajax({
     }
 });
 stimuli_list = jsPsych.randomization.repeat(stimuli_list, 1);
-
-var psudo = {
-    type: 'survey-text',
-    questions: [
-        {prompt: "Enter number of psudorandom audio files for experiment", name: 'PsudoSize',
-        placeholder: "Enter number of audio files for experiment"
-        },
-    ],
-    on_finish: function (sizes) {
-        size = parseInt(sizes.PsudoSize);
-        if(size < stimuli_list.length){
-            stimuli_list = stimuli_list.slice(0, size-1);
-        }
-        else{
-            alert("The entered number of psudorandom audio files ("+size+") exceeds the number of available files. It will default to maximum number of files ("+stimuli_list.length+").")
-        }
-    }
-};
-
-/* Toggle for all audio files OR pseudorandom sample */
-var toggle_audio = {
-    type: 'html-button-response',
-    timeline: [toggle_audio, psudo],
-    stimulus: '<p>Please choose how do you want to toggle the audio in the experiment.</p>',
-    choices: ['Play all audio files', 'Play psudorandom files'],
-    on_finish: function (data) {
-        if(data.button_pressed == 0){
-            jsPsych.endCurrentTimeline();
-        }
-    }
-};
-timeline.push(toggle_audio);
-
-
-
 
 /* Javascript runs on the client side, and using javascript alone cannot access folders / files on the server side.
 Also, pavlovia does not allow PHP, jQuery or xmlhttprequest() to access server side folders / files for security reasons.
@@ -173,6 +128,17 @@ var check_consent = function (elem) {
 };
 
 
+
+
+/* create timeline */
+var timeline = [];
+
+/* init connection with pavlovia.org */
+var pavlovia_init = {
+    type: "pavlovia",
+    command: "init"
+};
+timeline.push(pavlovia_init);
 
 /* generate a random subject ID with 16 characters */
 //var participant_id = jsPsych.randomization.randomID(16);
@@ -335,6 +301,40 @@ var check_loop_node = {
     },
 }
 timeline.push(check_loop_node)
+
+var psudo = {
+    type: 'survey-text',
+    questions: [
+        {prompt: "Enter number of psudorandom audio files for experiment", name: 'PsudoSize',
+        placeholder: "Enter number of audio files for experiment"
+        },
+    ],
+    on_finish: function (sizes) {
+        size = parseInt(sizes.PsudoSize);
+        if(size < stimuli_list.length){
+            console.log("old_size"+stimuli_list.length);
+            stimuli_list = stimuli_list.slice(0, size-1);
+            console.log("new_size"+stimuli_list.length);
+        }
+        else{
+            alert("The entered number of psudorandom audio files ("+size+") exceeds the number of available files. It will default to maximum number of files ("+stimuli_list.length+").")
+        }
+    }
+};
+
+/* Toggle for all audio files OR pseudorandom sample */
+var toggle_audio = {
+    type: 'html-button-response',
+    timeline: [toggle_audio, psudo],
+    stimulus: '<p>Please choose how do you want to toggle the audio in the experiment.</p>',
+    choices: ['Play all audio files', 'Play psudorandom files'],
+    on_finish: function (data) {
+        if(data.button_pressed == 0){
+            jsPsych.endCurrentTimeline();
+        }
+    }
+};
+timeline.push(toggle_audio);
 
 /*switch to full screen*/
 var fullscreen_trial = {
